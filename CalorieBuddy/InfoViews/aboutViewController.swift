03feb4c82dcb2b8af789802fed11e88fd08ec7ce
Khushiyant <khushiyant2002@gmail.com>
2022-07-28 01:22:19 +0530

    add: about view controller

--- CalorieBuddy/InfoViews/aboutViewController.swift
+++ CalorieBuddy/InfoViews/aboutViewController.swift
@@ -9,29 +9,51 @@ import Foundation
 import UIKit
 
 class aboutViewController : UIViewController {
-    
-    private let header : appHeader = {
-        let head = appHeader()
-        head.translatesAutoresizingMaskIntoConstraints = false
+
+    private let filePath : String = "aboutUs"
+    private let aboutUs : UILabel = {
+       
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "About Us"
         
-        return head
+        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 35)
+        return label
+    }()
+    
+    private let aboutContent : UILabel = {
+       
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: "AppleSDGothicNeo-Medium", size: 18)
+        return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         
-        view.addSubview(header)
-        
+        view.addSubview(aboutUs)
+        view.addSubview(aboutContent)
         setupLayout()
         
     }
     fileprivate func setupLayout(){
-//        Header Configuration
-        header.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        header.widthAnchor.constraint(equalTo: view.widthAnchor).isActive  = true
-        header.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.15).isActive = true
         
+//        About me label
+        aboutUs.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        aboutUs.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+//      About us content
+        aboutContent.topAnchor.constraint(equalTo: aboutUs.bottomAnchor, constant: 20).isActive = true
+        aboutContent.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        aboutContent.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        aboutContent.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        aboutContent.text = "Hello Everyone"
+                 
+        
+
         
     }
 }
